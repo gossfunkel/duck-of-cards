23e269d0dc5584c872bd6266fed6da1ea6b642ac
Gossfunkel <katherineegoss@gmail.com>
2025-07-28 02:35:12 +0100

    player castle hp implemented, enemies do dmg on arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import WindowProperties, NodePath, loadPrcFileData, BitMask32, Vec3, DirectionalLight
+from panda3d.core import WindowProperties, NodePath, loadPrcFileData, BitMask32, Vec3
+from panda3d.core import DirectionalLight, TextNode, LMatrix4
 from direct.interval.IntervalGlobal import *
 from direct.interval.LerpInterval import LerpPosInterval
 from enum import Enum
@@ -15,15 +16,37 @@ threading-model Cull/Draw
 
 loadPrcFileData("", config_vars)
 
+castleHP = 100
+
 class PlayerCastle():
 	def __init__(self, sb):
-		self.hp = 100;
+		self.model = sb.loader.loadModel("playerBase.gltf")
+		self.model.reparentTo(render)
+		self.model.setScale(0.2)
+		self.model.setPos(0.,0.,1.)
+		self.model.setColor(0.3,0.35,0.6,1.)
 
-		self.castleModel = sb.loader.loadModel("playerBase.gltf")
-		self.castleModel.reparentTo(render)
-		self.castleModel.setScale(0.2)
-		self.castleModel.setPos(0.,0.,1.)
-		self.castleModel.setColor(0.3,0.35,0.6,1.)
+class Enemy():
+	def __init__(self, enemyNode, pos):
+		self.node = enemyNode
+		self.node.setPos(pos)
+		self.move = self.node.posInterval(10., Vec3(0,0,0), pos)
+		self.despawnInt = Func(self.despawn)
+		self.moveSeq = Sequence(
+			self.move,
+			self.despawnInt
+		)
+		#self.move = LerpPosInterval(self.node, 15, Vec3(0,0,0),
+		#								startPos=pos, blendType='noBlend', fluid=1)
+		#self.move.setDoneEvent(Interval(Func(self.despawn),0.))
+		self.moveSeq.start()
+
+	def despawn(self):
+		global castleHP
+
+		print(str(self.node) + " despawning")
+		castleHP -= 5
+		# and do a wee animation?
 
 class DuckOfCards(ShowBase):
 	def __init__(self):
@@ -36,13 +59,20 @@ class DuckOfCards(ShowBase):
 		self.spawner = {1: Vec3(0.,10.,0.), 2: Vec3(10.,0.,0.), 3: Vec3(0.,-10.,0.), 4: Vec3(-10.,0.,0.)}
 
 		self.set_background_color(0.1,0.4,0.2,1.)
+		self.castleHPdisplay = TextNode('castle HP display')
+		self.castleHPdisplay.setText(str(castleHP))
+		self.castleHPdisplay.setFrameColor(0, 0, 0, 1)
+		self.castleHPdisplay.setFrameAsMargin(0.2, 0.2, 0.1, 0.1)
+		self.textNodePath = aspect2d.attachNewNode(self.castleHPdisplay)
+		self.textNodePath.setScale(0.1)
+		self.textNodePath.setPos(-0.95,0., 0.8)
 
-		dirLight 	= DirectionalLight('dirLight')
-		dirLight.setColorTemperature(6950)
-		dirLight.setShadowCaster(True, 512, 512)
-		dirLightNp  = render.attachNewNode(dirLight)
-		dirLightNp.setHpr(0,-70,135)
-		render.setLight(dirLightNp)
+		self.dirLight 	= DirectionalLight('dirLight')
+		self.dirLight.setColorTemperature(6950)
+		self.dirLight.setShadowCaster(True, 512, 512)
+		self.dirLightNp  = render.attachNewNode(self.dirLight)
+		self.dirLightNp.setHpr(0,-70,135)
+		render.setLight(self.dirLightNp)
 
 		self.tileModel = self.loader.loadModel("box")
 		self.tileModel.setColor(0.,0.3,0.,1.)
@@ -74,6 +104,8 @@ class DuckOfCards(ShowBase):
 	def update(self, task):
 		dt = globalClock.getDt()
 
+		self.castleHPdisplay.setText(str(castleHP))
+
 		#for enemy in self.enemies:
 		#	#print(enemy)
 		#	enemy.setPos(enemy.getPos()-Vec3(0.,0.1*dt,0.))
@@ -93,23 +125,18 @@ class DuckOfCards(ShowBase):
 				counter += 1
 
 	def spawnEnemy(self, pos):
-		newEnemy = self.enemyModelNd.attachNewNode("enemy " + str(self.enemyCount))
-		newEnemy.setPos(pos)
-		self.enemyModel.instanceTo(newEnemy)
+		newEnNd = self.enemyModelNd.attachNewNode("enemy " + str(self.enemyCount))
+		newEnemy = Enemy(newEnNd, pos)
+		self.enemyModel.instanceTo(newEnemy.node)
 		self.enemyCount += 1
 		self.enemies.append(newEnemy)
-		self.moveEnemy = LerpPosInterval(newEnemy,
-                    15,
-                    Vec3(0,0,0),
-                    startPos=pos,
-                    blendType='noBlend',
-                    fluid=1)
-		self.moveEnemy.start()
 
 	def spawnEnemyWave(self, num, pos):
-		self.spawnSeq = Sequence(Func(self.spawnEnemy,pos), Wait(2.5)) 
+		self.spawnSeq = Sequence() 
 		for _ in range(num):
-			self.spawnSeq.loop()
+			self.spawnSeq.append(Func(self.spawnEnemy,pos))
+			self.spawnSeq.append(Wait(2.5))
+		self.spawnSeq.start()
 
 app = DuckOfCards()
 app.run()
