405ca1d32dfa47695150ce2f9e0380df0dab0fcf
Gossfunkel <katherineegoss@gmail.com>
2025-07-29 21:29:04 +0100

    this collision detection system is quite confusing

--- main.py
+++ main.py
@@ -92,9 +92,11 @@ class Enemy():
 		self.hp = 20
 		# CollisionCapsule(ax, ay, az, bx, by, bz, radius)
 		self.hitSphere = CollisionCapsule(0.09, -0.1, 1.2,0.09, -0.1, 1.45, .12)
-		self.hitNp = self.node.attachNewNode(CollisionNode('{}-cnode'.format(str(self.node))))
+		hcnode = CollisionNode('{}-cnode'.format(str(self.node)))
+		hcnode.setIntoCollideMask(BitMask32(0x02))
+		self.hitNp = self.node.attachNewNode(hcnode)
 		self.hitNp.node().addSolid(self.hitSphere)
-		#self.hitNp.show() 								# uncomment to show hitbox
+		self.hitNp.show() 								# uncomment to show hitbox
 
 		# testMaterial = Material()
 		# testMaterial.setShininess(3.0) # Make this material shiny
@@ -122,6 +124,18 @@ class Enemy():
 
 		self.node.removeNode() # clean up the node
 
+class Arrow():
+	def __init__(self, arrowNd, pos):
+		self.node = arrowNd
+		self.node.setPos(pos[0]-0.1, 
+						pos()[1]-0.1,
+						pos()[2]+1.0)
+		#projectileNp = render.attachNewNode(ActorNode('projectile'))
+		self.cnode = CollisionNode('arrowColNode')
+		self.cnode.setFromCollideMask(BitMask32(0x00))
+		self.fromObject = self.node.attachNewNode()
+		self.fromObject.node().addSolid(CollisionSphere(0, 0, 0, 0.5))
+
 class Tower():
 	def __init__(self, towerNode, pos):
 		self.node = towerNode
@@ -130,34 +144,45 @@ class Tower():
 		self.rateOfFire = 1.0
 		self.damage = 1.0
 		self.cooldown = 0.0
-		#self.projectilePusher = PhysicsCollisionHandler()
+
+		# initialise detection of enemies in range
 		self.rangeSphere = CollisionSphere(0, 0, 0, 4)
-		self.rangeColliderNp = self.node.attachNewNode(CollisionNode(str(self.node) + '-range-cnode'))
+		rcnode = CollisionNode(str(self.node) + '-range-cnode')
+		rcnode.setFromCollideMask(BitMask32(0x02))
+		self.rangeColliderNp = self.node.attachNewNode(rcnode)
 		self.rangeColliderNp.node().addSolid(self.rangeSphere)
-		#self.rangeColliderNp.show()
+		self.rangeColliderNp.show()
+
 		self.enemyDetector = CollisionTraverser(str(self.node) + '-enemy-detector')
 		self.detectorQueue = CollisionHandlerQueue()
 		self.enemyDetector.addCollider(self.rangeColliderNp, self.detectorQueue)
+		#self.enemyDetector.showCollisions(base.enemyModelNd)
 
-		base.taskMgr.add(self.attackLoop, "{}_attackLoop".format(self.node), taskChain='default')
+		base.taskMgr.add(self.attackLoop, str(self.node)+"_attackLoop", taskChain='default')
 
 	def attackLoop(self, task):
+		self.enemyDetector.traverse(base.enemyModelNd)
 		if (self.cooldown == 0):
-			if (self.detectorQueue.entries != None):
-				self.launchProjectiles()
-			self.cooldown += 5./self.rateOfFire
-			base.ui.showCooldown(self.node.getPos(), self.cooldown)
+			print("off cooldown")
+			if (len(self.detectorQueue.entries) != 0):
+				print("enemy detected!")
+				self.detectorQueue.sortEntries()
+				if (self.detectorQueue.entries[0].getIntoNodePath() == "**enemy**"):
+					print("firing at enemy")
+					self.launchProjectiles(self.detectorQueue.entries[0].getIntoNodePath())
+					self.cooldown += 5./self.rateOfFire
+					base.ui.showCooldown(self.node.getPos(), self.cooldown)
 		else:
 			base.ui.showCooldown(self.node.getPos(), self.cooldown)
 			self.cooldown -= .05
+
 		task.cont
 
-	def launchProjectiles(self):
-		target = self.detectorQueue.entries[0]
-		projectileNp = render.attachNewNode(ActorNode('projectile'))
-		fromObject = projectileNp.attachNewNode(CollisionNode('projectileColNode'))
-		fromObject.node().addSolid(CollisionSphere(0, 0, 0, 1))
-		self.projectilePusher.addCollider(fromObject, projectileNp)
+	def launchProjectiles(self, target):
+		print("firing at " + str(target))
+		arrowNd = base.arrowModelNd.attachNewNode("arrow")
+		newArrow = Arrow(arrowNd, self.node.getPos())
+		base.arrowModel.instanceTo(arrowNd)
 
 class DuckOfCards(ShowBase):
 	def __init__(self):
@@ -212,9 +237,10 @@ class DuckOfCards(ShowBase):
 		self.tilePicker = CollisionTraverser()
 		self.tpQueue = CollisionHandlerQueue()
 		tpNode = CollisionNode('tp-node')
-		tpNode.setFromCollideMask(BitMask32.bit(1))
+		tpNode.setFromCollideMask(BitMask32(0x01))
 		tpNp = self.cam.attachNewNode(tpNode)
 		self.pickerRay = CollisionRay()
+		#self.pickerRay.setCollideMask(0)
 		tpNode.addSolid(self.pickerRay)
 		self.tilePicker.addCollider(tpNp, self.tpQueue)
 		self.hitTile = False
@@ -222,6 +248,11 @@ class DuckOfCards(ShowBase):
 
 		# create a player castle object
 		self.castle = PlayerCastle(self)
+
+		# initialise projectile models
+		self.arrowModel = self.loader.loadModel("arrow1.gltf")
+		self.arrowModelNd = self.render.attachNewNode("arrow-models")
+		self.arrowModel.setScale(0.05)
 
 		# initialise tower models
 		self.towerCount = 0
@@ -308,7 +339,7 @@ class DuckOfCards(ShowBase):
 				tile = self.tileMap.attachNewNode("tile-" + str(counter))
 				tile.setPos(width/2 - (width-x),length/2 - (length-y),0.)
 				self.tileModel.instanceTo(tile)
-				tile.find("**/box").node().setIntoCollideMask(BitMask32.bit(1))
+				tile.find("**/box").node().setIntoCollideMask(BitMask32(0x01))
 				counter += 1
 
 	def spawnEnemy(self, pos):
